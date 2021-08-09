f1a813976d7c02fc8f9f8e4c3b99d7cc9192fd3f
dennuguyen <dennuguyen@gmail.com>
2021-08-09 18:45:17 +1000

    Rename file

---
+++ controllers/Puck_You_MTRN4110_PhaseD/include/KinematicModel.hpp
@@ -0,0 +1,12 @@
+#ifndef KINEMATIC_MODEL_HPP
+#define KINEMATIC_MODEL_HPP
+
+namespace mtrn4110 {
+namespace epuck {
+constexpr auto wheelRadius = 0.02;
+constexpr auto axleLength = 0.0566;
+constexpr auto maxMotorSpeed = 6.28;
+}  // namespace epuck
+}  // namespace mtrn4110
+
+#endif  // KINEMATIC_MODEL_HPP
