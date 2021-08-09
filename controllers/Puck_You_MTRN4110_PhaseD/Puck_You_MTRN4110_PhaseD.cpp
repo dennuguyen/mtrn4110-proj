0a73b2221636aa3af0e48be8a38ee063dae042f4
dennuguyen <dennuguyen@gmail.com>
2021-08-10 00:54:32 +1000

    Void unused arguments

--- controllers/Puck_You_MTRN4110_PhaseD/Puck_You_MTRN4110_PhaseD.cpp
+++ controllers/Puck_You_MTRN4110_PhaseD/Puck_You_MTRN4110_PhaseD.cpp
@@ -16,13 +16,16 @@ static auto realtimeSteps(mtrn4110::TaskControl &taskControl) -> void {
     // constexpr auto csvPath = "../../MotionExecution.csv";
     // constexpr auto mapPath = "../../Map.txt";
     // constexpr auto pathPlanPath = "../../PathPlan.txt";
-
+    (void)taskControl;
     // Enter control loop.
     while (1) {
     }
 }
 
 auto main(int argc, char **argv) -> int {
+    (void)argc;
+    (void)argv;
+
     // Instantiate webots robot.
     auto robot = webots::Robot();
 
