ad16400550fee9f5e944874559db8465b90212d9
dennuguyen <dennuguyen@gmail.com>
2021-08-10 00:45:59 +1000

    Fix use of references

--- controllers/Puck_You_MTRN4110_PhaseD/src/Timer.cpp
+++ controllers/Puck_You_MTRN4110_PhaseD/src/Timer.cpp
@@ -5,11 +5,11 @@ namespace mtrn4110 {
 Timer::Timer(webots::Robot const& robot) : robot_(robot), ref_(), exp_() {}
 
 auto Timer::time(double expiry) noexcept -> void {
-    ref_ = robot_->getTime();
+    ref_ = robot_.getTime();
     exp_ = expiry;
 }
 
-auto const Timer::expired() const noexcept -> bool { return (robot_->getTime() - ref_) > exp_; }
+auto const Timer::expired() const noexcept -> bool { return (robot_.getTime() - ref_) > exp_; }
 
 auto operator<<(std::ostream& os, Timer const& timer) noexcept -> std::ostream& {
     os << "Time: " << timer.ref_ << std::endl;
