abdcb61d2b01cb74d56bdcc0596708069c3c2c52
dennuguyen <dennuguyen@gmail.com>
2021-08-12 02:53:22 +1000

    Instantiate EPuck kinematic model

--- controllers/Puck_You_MTRN4110_PhaseD/include/KinematicModel.hpp
+++ controllers/Puck_You_MTRN4110_PhaseD/include/KinematicModel.hpp
@@ -1,11 +1,25 @@
 #ifndef KINEMATIC_MODEL_HPP
 #define KINEMATIC_MODEL_HPP
 
-namespace mtrn4110 {
-class KinematicModel {
-   public:
-   private:
+namespace {
+struct KinematicModel {
+    KinematicModel(double wheelRadius, double axleLength, double maxSpeed, double maxMotorSpeed)
+    : wheelRadius(wheelRadius)
+    , axleLength(axleLength)
+    , maxSpeed(maxSpeed)
+    , maxMotorSpeed(maxMotorSpeed) {}
+
+    double const wheelRadius;
+    double const axleLength;
+    double const maxSpeed;
+    double const maxMotorSpeed;
 };
+}  // namespace
+
+namespace mtrn4110 {
+namespace model {
+auto ePuck = KinematicModel(0.02, 0.0566, 0.1287, 6.28);
+}  // namespace model
 }  // namespace mtrn4110
 
 #endif  // KINEMATIC_MODEL_HPP
