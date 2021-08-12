bd9c208808dac07a8109f1109610c2fd1f5cc920
James Jiang <z5206109@unsw.edu.au>
2021-08-12 10:00:43 +1000

    modified map

--- controllers/Puck_You_MTRN4110_PhaseD/include/Util.hpp
+++ controllers/Puck_You_MTRN4110_PhaseD/include/Util.hpp
@@ -5,13 +5,18 @@
 #include <utility>
 #include <vector>
 
+#include "Node.hpp"
+
+const int N_ROWS = 5;
+const int N_COLS = 9;
+
 namespace mtrn4110 {
 namespace defaultType {
 using HeadingType = int;
 using PoseType = std::pair<int, int>;
 using VelocityType = double;
 using MotionType = std::pair<PoseType, VelocityType>;
-using GraphType = std::map<PoseType, std::pair<int, std::vector<PoseType>>>;
+using GraphType = std::array<std::array<Node, N_COLS>, N_ROWS>;
 }  // namespace defaultType
 
 namespace file {
