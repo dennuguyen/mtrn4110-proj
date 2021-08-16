c025e082eaf9ded7741e21f507b021391b66d263
dan <dennuguyen@gmail.com>
2021-08-17 01:03:47 +1000

    Write keyboard

---
+++ controllers/Puck_You_MTRN4110_PhaseD/include/SimpleTeleoperation.hpp
@@ -0,0 +1,44 @@
+#ifndef SIMPLE_TELEOPERATION_HPP
+#define SIMPLE_TELEOPERATION_HPP
+
+#include <memory>
+#include <vector>
+
+#include <webots/Keyboard.hpp>
+#include <webots/Robot.hpp>
+
+#include "Deliberator.hpp"
+#include "Util.hpp"
+
+namespace mtrn4110 {
+
+// A deliberator to teleoperate the robot based on input motion commands. Teleoperation is based on
+// WQE which corresponds to FLR. The Webots keyboard is non-blocking.
+template<typename DeliberateType = char>
+class SimpleTeleoperation final : public Deliberator<DeliberateType> {
+   public:
+    SimpleTeleoperation(webots::Robot& robot)
+    : Deliberator<DeliberateType>()
+    , keyboard_(robot.getKeyboard()) {
+        keyboard_->enable(robot.getBasicTimeStep());
+    }
+
+    auto readInput() -> void {
+        switch (keyboard_->getKey()) {
+        case 'W': this->delib_ = 'F';
+        case 'Q': this->delib_ = 'L';
+        case 'E': this->delib_ = 'R';
+        default: break;
+        }
+    }
+
+   private:
+    auto print(std::ostream& os) const noexcept -> void {
+        os << this->delib_ << std::endl;
+    }
+
+    std::unique_ptr<webots::Keyboard> keyboard_;
+};
+}  // namespace mtrn4110
+
+#endif  // SIMPLE_TELEOPERATION_HPP
