c9b1b509a002504475bd7397b1b6ba68232d0a38
dan <dennuguyen@gmail.com>
2021-08-16 23:25:08 +1000

    Throw invalid argument

--- controllers/Puck_You_MTRN4110_PhaseD/src/TaskControl.cpp
+++ controllers/Puck_You_MTRN4110_PhaseD/src/TaskControl.cpp
@@ -9,40 +9,39 @@ namespace mtrn4110 {
 
 TaskControl::TaskControl(int numLocks, int numTimers)
 : numLocks_(numLocks)
-, numTimers_(numTimers) {
-}
+, numTimers_(numTimers) {}
 
 auto TaskControl::acquireLock(int lock) -> void {
     if (lock < 0 || numLocks_ < lock) {
-        throw std::runtime_error("Invalid lock number");
+        throw std::invalid_argument("Invalid lock number");
     }
     locks_.at(lock) = true;
 }
 
 auto TaskControl::releaseLock(int lock) -> void {
     if (lock < 0 || numLocks_ < lock) {
-        throw std::runtime_error("Invalid lock number");
+        throw std::invalid_argument("Invalid lock number");
     }
     locks_.at(lock) = false;
 }
 
 auto TaskControl::isLockBusy(int lock) const -> bool {
     if (lock < 0 || numLocks_ < lock) {
-        throw std::runtime_error("Invalid lock number");
+        throw std::invalid_argument("Invalid lock number");
     }
     return locks_.at(lock);
 }
 
 auto TaskControl::time(int timer, double duration) -> void {
     if (timer < 0 || numLocks_ < timer) {
-        throw std::runtime_error("Invalid timer number");
+        throw std::invalid_argument("Invalid timer number");
     }
     timers_.at(timer).time(duration);
 }
 
 auto TaskControl::expired(int timer) const -> bool {
     if (timer < 0 || numLocks_ < timer) {
-        throw std::runtime_error("Invalid timer number");
+        throw std::invalid_argument("Invalid timer number");
     }
     return timers_.at(timer).expired();
 }
