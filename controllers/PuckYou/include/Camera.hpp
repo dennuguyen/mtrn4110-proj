b313be36325b1232f332bafb023723b3af48263e
dan <dennuguyen@gmail.com>
2021-08-18 06:20:35 +1000

    Write Camera class

--- controllers/PuckYou/include/Camera.hpp
+++ controllers/PuckYou/include/Camera.hpp
@@ -1,24 +1,28 @@
-#include <webots/DistanceSensor.hpp>
-#include <webots/Motor.hpp>
-#include <webots/Robot.hpp>
-#include <webots/Keyboard.hpp>
-#include <webots/GPS.hpp>
-#include <webots/InertialUnit.hpp>
+#ifndef CAMERA_HPP
+#define CAMERA_HPP
+
+#include <memory>
+
 #include <webots/Camera.hpp>
+#include <webots/Robot.hpp>
 
+namespace mtrn4110 {
+class Camera {
+   public:
+    Camera(webots::Robot& robot)
+    : camera_(robot.getCamera("camera")) {
+        camera_->enable(robot.getTimeStep());
+    }
 
-#define TIME_STEP 64
-using namespace webots;
+    auto saveImage(std::string const& outputFile, int quality) const -> void {
+        if (camera->saveImage(outputFile, quality) != 0) {
+            throw std::runtime_error("Could not save image to " + outputFile);
+        }
+    }
 
-int main(int argc, char **argv) {
-  Robot *robot = new Robot();
-  
-  
-  Camera *cm;
-  cm=robot->getCamera("camera");
-  cm->enable(TIME_STEP);
-  
-  
-  delete robot;
-  return 0;  // EXIT_SUCCESS
-}
+   private:
+    std::unique_ptr<webots::Camera> camera_;
+};
+}  // namespace mtrn4110
+
+#endif  // CAMERA_HPP
