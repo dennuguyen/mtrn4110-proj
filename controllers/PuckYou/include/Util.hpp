69e831cb7749a49b948f5c7e5ba460b0ded9cad5
dan <dennuguyen@gmail.com>
2021-08-18 19:44:07 +1000

    Clean up file paths

--- controllers/PuckYou/include/Util.hpp
+++ controllers/PuckYou/include/Util.hpp
@@ -26,9 +26,9 @@ using GraphType =
 }  // namespace defaultTypes
 
 namespace files {
-auto constexpr mazeImage = "Maze.png";
-auto constexpr robotImage = "Robot_2.png";
-auto constexpr ladybugImage = "Ladybug_small.png";
+auto constexpr mazeImage = "../../images/Maze_2.png";
+auto constexpr robotImage = "../../images/Robot_2.png";
+auto constexpr ladybugImage = "../../images/Ladybug_small.png";
 }  // namespace files
 }  // namespace mtrn4110
 
