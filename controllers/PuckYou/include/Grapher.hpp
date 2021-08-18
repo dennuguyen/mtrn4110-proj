2b40bdb437625310d7058b41c845071ede7abe22
dan <dennuguyen@gmail.com>
2021-08-19 04:05:56 +1000

    Pull out Map elements

--- controllers/PuckYou/include/Grapher.hpp
+++ controllers/PuckYou/include/Grapher.hpp
@@ -2,43 +2,29 @@
 #define GRAPHER_HPP
 
 #include <map>
-#include <regex>
 
 #include "Util.hpp"
 
 namespace mtrn4110 {
-// An interface for a generic grapher.
-template<typename MapType = defaultTypes::MapType, typename GraphType = defaultTypes::GraphType>
+// An interface for a grapher.
+template<typename GraphType = defaultTypes::GraphType>
 class Grapher {
    public:
-    class Graph {
-       public:
-       private:
-    };
-
-    // Constructor to initialise graph.
-    Grapher()
-    : map_() {}
-
-    // Read the map from file name.
-    auto readMap(std::string const& map) -> void {
-        auto const delimiter = std::regex("\n");
-        map_ = std::vector<std::string>(
-            std::sregex_token_iterator(map.begin(), map.end(), delimiter, -1),
-            std::sregex_token_iterator());
-    }
+    // Default constructor.
+    Grapher() = default;
 
     // Build the graph from the read-in map.
-    auto buildGraph() -> GraphType {
-        if (map_.empty() == true) {
+    auto buildGraph(std::string const& map) -> GraphType {
+        if (map.empty() == true) {
             throw std::runtime_error("Cannot build graph from empty map.");
         }
 
-        auto graph =
-            std::map<std::pair<int, int>, std::pair<int, std::vector<std::pair<int, int>>>>();
+        // Tokenise the map string for [][] access operator.
+        auto tokenisedMap = util::tokenise(map, '\n');
 
-        auto const& maxLine = static_cast<int>(map_.size());
-        auto const& maxColumn = static_cast<int>(map_.at(0).size());
+        auto graph = GraphType();
+        auto const& maxLine = static_cast<int>(tokenisedMap.size());
+        auto const& maxColumn = static_cast<int>(tokenisedMap.at(0).size());
         for (auto line = 0; line < maxLine; line++) {
             for (auto col = 0; col < maxColumn; col++) {
                 // At centre of tile.
@@ -47,7 +33,7 @@ class Grapher {
                     auto const y = (line - 1) / 2;
 
                     // Check start and end position.
-                    // switch (map_[line][col]) {
+                    // switch (tokenisedMap[line][col]) {
                     // case ' ': break;
                     // case 'x': end_ = {x, y}; break;
                     // case '^':
@@ -71,7 +57,7 @@ class Grapher {
 
                     // Check vertical wall to right of centre of tile.
                     if (col + 2 < maxColumn) {
-                        if (map_[line][col + 2] == ' ') {
+                        if (tokenisedMap[line][col + 2] == ' ') {
                             graph[{x, y}].first = unvisited;
                             graph[{x, y}].second.emplace_back(x + 1, y);
                             graph[{x + 1, y}].first = unvisited;
@@ -81,7 +67,7 @@ class Grapher {
 
                     // Check horizontal wall below centre of tile.
                     if (line + 1 < maxLine) {
-                        if (map_[line + 1][col] == ' ') {
+                        if (tokenisedMap[line + 1][col] == ' ') {
                             graph[{x, y}].first = unvisited;
                             graph[{x, y}].second.emplace_back(x, y + 1);
                             graph[{x, y + 1}].first = unvisited;
@@ -93,21 +79,6 @@ class Grapher {
         }
 
         return graph;
-    }
-
-    // Operator overload for <<.
-    friend auto operator<<(std::ostream& os, Grapher const& grapher) noexcept -> std::ostream& {
-        grapher.print(os);
-        return os;
-    }
-
-   protected:
-    MapType map_;
-
-   private:
-    // Write any required data to an output stream.
-    auto print(std::ostream& os) const noexcept -> void {
-        std::for_each (map_.begin(), map_.end(), [&os](auto const& i) { os << i << std::endl; });
     }
 };
 }  // namespace mtrn4110
