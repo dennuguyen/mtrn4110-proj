3356b083f59ebc9b029174f025dc973cbe6f6317
dan <dennuguyen@gmail.com>
2021-08-20 21:58:01 +1000

    Create mouse control loop

---
+++ controllers/PuckYou/Mouse.cpp
@@ -0,0 +1,90 @@
+#include <Python.h>
+
+#include <iostream>
+#include <thread>
+
+#include <webots/Robot.hpp>
+
+// Used exclusively in autonomous control.
+#include "BFSDFS.hpp"
+#include "CVProcessor.hpp"
+#include "Camera.hpp"
+#include "Grapher.hpp"
+#include "PathSequencer.hpp"
+
+// Used in both teleoperation and autonomous control.
+#include "DeadReckoning.hpp"
+#include "EPuckMotionPlanner.hpp"
+#include "MotorController.hpp"
+#include "SimpleTeleoperation.hpp"
+
+// Extra control over control loop.
+#include "TaskControl.hpp"
+
+// Perform simulation steps until Webots is stopping the controller.
+static auto simulationSteps(webots::Robot& robot) -> void {
+    auto const timeStep = robot.getBasicTimeStep();
+    while (robot.step(timeStep) != -1) {
+    }
+}
+
+// Perform real-time steps.
+// This function contains the control loop logic for the EPuck. It supports only teleoperation.
+static auto realTimeSteps(webots::Robot& robot) -> void {
+    // Instantiate our task controller class.
+    auto taskControl = mtrn4110::TaskControl(robot, 1, 0);
+    auto constexpr motionLock = 0;  // true = in motion, false = not in motion
+
+    // These RSA elements are used in both teleoperation and autonomous control.
+    auto teleoperation = mtrn4110::SimpleTeleoperation(robot);
+    auto trajectoryPlanner = mtrn4110::DeadReckoning('\0');
+    auto motionPlanner = mtrn4110::EPuckMotionPlanner();
+    auto motorController = mtrn4110::MotorController(robot);
+
+    // Enter control loop.
+    while (1) {
+        auto motion = '\0';
+
+        // Get teleoperation motion command.
+        auto const key = teleoperation.readInput();
+
+        // Calculate the trajectory.
+        trajectoryPlanner.updateMotion(motion);
+        trajectoryPlanner.computeTrajectory({0.01, 0, 0}, {0, 0, 0.2});
+
+        // Calculate the motor setpoints for current trajectory.
+        auto const angle = trajectoryPlanner.getAngle();
+        auto const distance = trajectoryPlanner.getDistance();
+        auto const linearVelocity = trajectoryPlanner.getLinearVelocity();
+        auto const angularVelocity = trajectoryPlanner.getAngularVelocity();
+        motionPlanner.computeMotorSetpoints(angle, distance, linearVelocity, angularVelocity);
+
+        // Feed motor setpoints to motor controller.
+        motorController.setPosition(motionPlanner.getMotorPositions());
+        motorController.setVelocity(motionPlanner.getMotorVelocities());
+        taskControl.acquireLock(motionLock);
+
+        // Do not do anything while in motion.
+        while (taskControl.isLockBusy(motionLock) == true) {
+            if (motorController.isAtPosition() == true) {
+                taskControl.releaseLock(motionLock);
+                // taskControl.wait(motionTimer, 0.01);  // Wait after motion is completed.
+            }
+        }
+    }
+}
+
+auto main(int argc, char** argv) -> int {
+    // Instantiate webots robot.
+    auto robot = webots::Robot();
+
+    // Spin threads.
+    auto t1 = std::thread(simulationSteps, std::ref(robot));
+    auto t2 = std::thread(realTimeSteps, std::ref(robot));
+
+    // Wait for threads to finish.
+    t1.join();
+    t2.join();
+
+    return 0;
+}
