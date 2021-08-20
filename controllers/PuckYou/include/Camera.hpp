26826312b7b710da0a13aeaa545fdeede97ee754
dan <dennuguyen@gmail.com>
2021-08-20 18:25:28 +1000

    Leave camera enabled

--- controllers/PuckYou/include/Camera.hpp
+++ controllers/PuckYou/include/Camera.hpp
@@ -10,23 +10,21 @@ namespace mtrn4110 {
 class Camera {
    public:
     Camera(webots::Robot& robot)
-    : robot_(robot)
-    , camera_(robot.getCamera("bird")) {}
+    : timeStep_(robot.getBasicTimeStep())
+    , camera_(robot.getCamera("bird")) {
+        camera_->enable(timeStep_);
+    }
 
     // Take an image of the world from the robot's camera and save it to the outputFile. Camera is
     // only enabled when used.
     auto snap(std::string const& outputFile, int quality) const -> void {
-        camera_->enable(robot_.getBasicTimeStep());
-        robot_.step(robot_.getBasicTimeStep());  // Let camera enable.
         if (camera_->saveImage(outputFile, quality) != 0) {
             throw std::runtime_error("Could not save image to " + outputFile);
         }
-        camera_->disable();
-        robot_.step(robot_.getBasicTimeStep());  // Let camera disable.
     }
 
    private:
-    webots::Robot& robot_;
+    int const timeStep_;
     std::unique_ptr<webots::Camera> camera_;
 };
 }  // namespace mtrn4110
